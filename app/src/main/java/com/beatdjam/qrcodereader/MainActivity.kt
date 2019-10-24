51f630b3f0e9522b71178aa2040dc5ff0807e822
beatdjam <beat3526@gmail.com>
2019-10-24 23:53:30 +0900

    [modify]Open the loaded URL in Chrome

--- app/src/main/java/com/beatdjam/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/beatdjam/qrcodereader/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.beatdjam.qrcodereader
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,21 +25,33 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         when {
-            result != null -> Toast.makeText(this, result.contents, Toast.LENGTH_LONG).show()
+            result != null -> openDialog(result.contents)
             else -> super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
+    private fun openDialog(contents: String) {
+        AlertDialog.Builder(this)
+            .setTitle("読み取ったURL")
+            .setMessage(contents)
+            .setPositiveButton("OK") { _, _ ->
+                openBrowser(contents)
+            }
+            .setNegativeButton("CLOSE", null)
+            .show()
+    }
+
+    private fun openBrowser(contents: String) {
+        val uri = Uri.parse(contents)
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
