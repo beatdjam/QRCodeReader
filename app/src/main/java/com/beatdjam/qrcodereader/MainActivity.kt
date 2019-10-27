8ae96dfcf61570833b954e7c62de15ff372ec870
beatdjam <beat3526@gmail.com>
2019-10-27 12:16:26 +0900

    Issue #1 #2 対応

--- app/src/main/java/com/beatdjam/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/beatdjam/qrcodereader/MainActivity.kt
@@ -3,8 +3,7 @@ package com.beatdjam.qrcodereader
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.webkit.URLUtil
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.zxing.integration.android.IntentIntegrator
@@ -23,38 +22,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        when {
-            result != null -> openDialog(result.contents)
-            else -> super.onActivityResult(requestCode, resultCode, data)
+        val result = IntentIntegrator
+            .parseActivityResult(requestCode, resultCode, data)
+            ?.contents
+        // resultが空なら何もせずに終了
+        if (result.isNullOrEmpty()) {
+            super.onActivityResult(requestCode, resultCode, data)
+            return
         }
+
+        // URLの場合のみWeb遷移用ダイアログを開く
+        if (URLUtil.isValidUrl(result)) openDialogForUrl(Uri.parse(result).toString())
+        else openDialogForOther(result)
     }
 
-    private fun openDialog(contents: String) {
+    private fun openDialogForUrl(url: String) {
         AlertDialog.Builder(this)
-            .setTitle("読み取ったURL")
-            .setMessage(contents)
-            .setPositiveButton("OK") { _, _ ->
-                openBrowser(contents)
-            }
-            .setNegativeButton("CLOSE", null)
+            .setTitle("読み取りURL")
+            .setMessage(url)
+            .setPositiveButton("はい") { _, _ -> openBrowser(url) }
+            .setNegativeButton("いいえ", null)
             .show()
     }
+
+    private fun openDialogForOther(contents: String) {
+        AlertDialog.Builder(this)
+            .setTitle("読み取りテキスト")
+            .setMessage(contents)
+            .setNegativeButton("閉じる", null)
+            .show()
+    }
+
 
     private fun openBrowser(contents: String) {
         val uri = Uri.parse(contents)
         startActivity(Intent(Intent.ACTION_VIEW, uri))
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
